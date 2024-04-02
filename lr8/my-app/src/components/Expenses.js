8cc0ca1a97a000302e6eced5595b9de67296c515
Dizzout <136970918+Dizzout@users.noreply.github.com>
2024-04-02 15:07:23 +0300

    Add files via upload

---
+++ lr8/my-app/src/components/Expenses.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+// Компонент ExpenseDate
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+// Компонент Card
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+
+// Компонент ExpenseItem
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+// Компонент Expenses
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+// Данные о расходах
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+// Экспортируем компонент Expenses
+export default function App() {
+  return <Expenses expenses={expenses} />;
+}
